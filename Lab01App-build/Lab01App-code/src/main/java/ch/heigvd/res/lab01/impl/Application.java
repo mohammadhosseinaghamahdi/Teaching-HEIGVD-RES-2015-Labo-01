d8e970ffc42c918965e551e99ad1858789936f0e
mohammad.aghamahd <mohammadhossein.aghamahdi@heig-vd.ch>
2015-03-16 07:52:52 +0100

    Test DFS passed

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Application.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Application.java
@@ -142,6 +142,7 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+          
       }
     });
   }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.LinkedList;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -16,7 +17,28 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // visiter le dossier
+     vistor.visit(rootDirectory);
+     
+     if (rootDirectory.listFiles() == null)
+         return;
+     
+     // visiter les fichiers
+     LinkedList<File> directories = new LinkedList<File>();
+     for (File file : rootDirectory.listFiles()) {
+         if (file.isDirectory())
+             directories.add(file);
+         else
+            vistor.visit(file);
+     }
+         
+     for (File directory : directories)
+         explore(directory, vistor);
+     // explorer les dossier
+    // vérifier si il n'y a pas de fichier
+      // on sorte
+      
+    // 
   }
 
 }
